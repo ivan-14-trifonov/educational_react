62947ceb25e86a166369e40397af33de33a9d148
Ivan_Trifonov <ivan14trifonov@gmail.com>
2023-06-24 18:53:19 +0300

    16:30 The first minimal example

--- src/index.tsx
+++ src/index.tsx
@@ -6,5 +6,8 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+    <div>
+      Приложение работает!
+      <button>Кнопка</button>
+    </div>
 );
